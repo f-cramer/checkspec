e53a932836067afe6deceb3ed5a87d12798535a5
Florian Cramer <florian.cramer@web.de>
2017-09-25 00:44:14 +0200

    fixed error where type parameter would not be found

--- checkspec.commons/src/main/java/checkspec/type/TypeVariableMatchableType.java
+++ checkspec.commons/src/main/java/checkspec/type/TypeVariableMatchableType.java
@@ -9,9 +9,9 @@ package checkspec.type;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,7 +98,7 @@ public class TypeVariableMatchableType extends AbstractMatchableType<TypeVariabl
 		GenericDeclaration genericDeclaration = variable.getGenericDeclaration();
 		TypeVariable<?>[] parameters = genericDeclaration.getTypeParameters();
 		return IntStream.range(0, parameters.length)
-				.filter(index -> parameters[index] == variable)
+				.filter(index -> parameters[index].equals(variable))
 				.findFirst()
 				.orElseThrow(() -> new IllegalArgumentException(String.format(NOT_FOUND, variable, genericDeclaration)));
 	}
