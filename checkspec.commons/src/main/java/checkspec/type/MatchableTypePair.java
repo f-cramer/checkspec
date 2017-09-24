e9bc907244d76c2908679f3a701d531c4b8b97ca
Florian Cramer <florian.cramer@web.de>
2017-09-24 16:53:32 +0200

    added @NonNull to fields in MatchablePair

--- checkspec.commons/src/main/java/checkspec/type/MatchableTypePair.java
+++ checkspec.commons/src/main/java/checkspec/type/MatchableTypePair.java
@@ -1,5 +1,7 @@
 package checkspec.type;
 
+import lombok.NonNull;
+
 /*-
  * #%L
  * CheckSpec Commons
@@ -9,9 +11,9 @@ package checkspec.type;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +33,8 @@ import lombok.Value;
 @Value
 public class MatchableTypePair {
 
+	@NonNull
 	private final Class<? extends MatchableType> left;
+	@NonNull
 	private final Class<? extends MatchableType> right;
 }
