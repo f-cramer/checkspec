8f84b460fc6e8c75d426dd82443967efbfe292da
Florian Cramer <florian.cramer@web.de>
2017-09-27 11:22:11 +0200

    added workaround for constructor.getGenericParameterTypes
    constructor.getGenericParameterTypes has no or less elements than
    constructor.getParameterTypes in some cases

--- checkspec.commons/src/main/java/checkspec/type/MatchableType.java
+++ checkspec.commons/src/main/java/checkspec/type/MatchableType.java
@@ -9,9 +9,9 @@ package checkspec.type;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -198,6 +198,11 @@ public interface MatchableType {
 	 * @return the matchable type
 	 */
 	static MatchableType forConstructorParameter(Constructor<?> constructor, int index) {
-		return forType(constructor.getGenericParameterTypes()[index]);
+		final Type[] genericParameterTypes = constructor.getGenericParameterTypes();
+		if (genericParameterTypes.length > index) {
+			return forType(genericParameterTypes[index]);
+		} else {
+			return forType(constructor.getParameterTypes()[index]);
+		}
 	}
 }
